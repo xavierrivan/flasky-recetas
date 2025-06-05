226d5f4a0dee88ade6f86fe7dfe3e9a5fa8f822c
Xavier Gordillo <xavier.gordillo@udla.edu.ec>
2025-06-04 21:25:55 -0500

    Corrige create_app para soportar Flask CLI y evitar KeyError en migraciones

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import config
+import os
+import flask.cli
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.login_view = 'main.login'
 
-def create_app(config_name):
+def create_app(config_name=None):
+    if config_name is None or isinstance(config_name, flask.cli.ScriptInfo):
+        config_name = os.getenv('FLASK_CONFIG') or 'default'
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
